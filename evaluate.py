f1010cbffabffb77026c9858557c32ba19f15c75
archiexdex <rc840327@gmail.com>
2021-09-06 23:57:45 +0800

    feat: - add quantitative analysis result in evaluate - add quantitative analysis function in utils

--- evaluate.py
+++ evaluate.py
@@ -5,7 +5,6 @@ from torch.utils.data import DataLoader
 from datetime import datetime
 from tqdm import tqdm
 import json
-from IQA_pytorch import SSIM, FSIM, DISTS, GMSD, MS_SSIM, LPIPSvgg, CW_SSIM, VIF
 from torchvision.utils import save_image
 
 from model import *
@@ -20,10 +19,9 @@ with open(f"{opt.cfg_dir}/{opt.no}.json", "r") as fp:
     opt = json.load(fp)
 opt = Namespace(**opt)
 same_seeds(opt.seed)
-try:
-    os.makedirs(f"{opt.rst_dir}/{opt.no}", 0o777)
-except:
-    pass
+if os.path.exists(f"{opt.rst_dir}/{opt.no}"):
+    shutil.rmtree(f"{opt.rst_dir}/{opt.no}")
+os.makedirs(f"{opt.rst_dir}/{opt.no}", 0o777)
 
 dataset = CTDataset(config=opt, isTrain=False)
 dataloader = DataLoader(dataset, batch_size=opt.batch_size, shuffle=False)
@@ -31,5 +29,14 @@ dataloader = DataLoader(dataset, batch_size=opt.batch_size, shuffle=False)
 model = Model(opt)
 model.load(load_mode)
 imgs = model.sample_image(dataset, sample_mode=sample_mode)
+
 for idx, img in imgs.items():
-    save_image(img, f"{opt.rst_dir}/{opt.no}/{idx}.png")
+    real_cb, real_fb, fake_fb, mask, grid = img
+    # Evaluate images 
+    origin_qa = get_qa(real_cb, real_fb, mask)
+    output_qa = get_qa(fake_fb, real_fb, mask)
+    # Write analysis to csv
+    save_csv(f"origin_{idx}", origin_qa, f"{opt.rst_dir}/{opt.no}")
+    save_csv(f"output_{idx}", output_qa, f"{opt.rst_dir}/{opt.no}")
+    # Save image results
+    save_image(grid, f"{opt.rst_dir}/{opt.no}/{idx}.png")
--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import numpy as np
 import random
 import argparse
 import json
+import csv
+from IQA_pytorch import SSIM, FSIM, DISTS, GMSD, MS_SSIM, LPIPSvgg, CW_SSIM, VIF
 
 def get_device():
     return torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -31,3 +33,69 @@ def save_config(config):
     _config = vars(config)
     with open(os.path.join(config.cfg_dir, f"{config.no}.json"), "w") as fp:
         json.dump(_config, fp, ensure_ascii=False, indent=4)
+
+# Get gray image
+def get_pixels_hu(info):
+    img = info.pixel_array
+    # Set outside-of-scan pixels to 0
+    # The intercept is usually -1024, so air is approximately 0
+    # img[img < -2000] = 0
+    # Convert to Hounsfield units (HU)
+    for idx, item in enumerate(info.PerFrameFunctionalGroupsSequence):
+        intercept = item.RealWorldValueMappingSequence[0].RealWorldValueIntercept
+        slope = item.RealWorldValueMappingSequence[0].RealWorldValueSlope
+        img[idx] = slope * img[idx].astype(np.float32) + intercept
+    return np.array(img, dtype='float32')
+
+def get_psnr(a, b):
+    dif = a - b
+    mse = dif ** 2
+    psnr = 10 * torch.log10(1 / mse.mean())
+    return psnr
+
+def get_qa(cb, fb, mk):
+    device = get_device()
+    trans = lambda x: x.unsqueeze(0)
+    get_ssim = SSIM().to(device)
+    get_fsim = FSIM().to(device)
+    get_gmsd = GMSD(1).to(device)
+    get_vif = VIF(channels=1).to(device)
+    get_msssim = MS_SSIM(channels=1).to(device)
+
+    cb = trans(cb)
+    fb = trans(fb)
+    mk = trans(mk)
+    
+    cb[mk<250] = 0
+    fb[mk<250] = 0
+    
+    ssim   = get_ssim  (cb, fb, False)[0].item()
+    psnr   = get_psnr  (cb, fb).item()
+    # TODO: 
+    fsim   = 0 #get_fsim  (cb, fb, False)[0].item()
+    gmsd   = get_gmsd  (cb, fb, False)[0].item()
+    msssim = get_msssim(cb, fb, False)[0].item()
+    vif    = get_vif   (cb, fb, False)[0].item()
+
+    return ssim, psnr, fsim, gmsd, msssim, vif
+
+def save_csv(name, qas, save_path):
+    fieldnames = ["name", "ssim", "psnr", "fsim", "gmsd", "msssim", "vif"]
+    if not os.path.exists(f"{save_path}/qa.csv"):
+        with open(f"{save_path}/qa.csv", "w") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
+    with open(f"{save_path}/qa.csv", "a+") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow({
+                fieldnames[0]: name,
+                fieldnames[1]: qas[0],
+                fieldnames[2]: qas[1],
+                fieldnames[3]: qas[2],
+                fieldnames[4]: qas[3],
+                fieldnames[5]: qas[4],
+                fieldnames[6]: qas[5],
+            })
+        
+
